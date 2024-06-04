94b8382cc5f24fbcb6d3e91139d2cba9919467eb
thgml21004 <thgml21004@gmail.com>
2024-06-04 12:15:08 +0900

    제이쿼로 탭 실행

--- 20240604/tab.js
+++ 20240604/tab.js
@@ -5,7 +5,7 @@ window.onload = function() {
 
             const siblings = this.parentElement.children; // 현재 요소와 함께 부모 요소의 다른 모든 자식 요소를 포함 // 나의 부모의 자식들 (html object)
             for (let sibling of siblings) {
-                sibling.classList.remove('on');
+                sibling.classList.remove = 'on';
             }
             // this.classList = 'on'; // 초기화
             // this.classList += ' on'; // 추가대입식
